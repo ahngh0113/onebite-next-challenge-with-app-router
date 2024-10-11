a137398e45a6b400088953432dfdcac5d97b8e97
안기환 <61266464+ahngh0113@users.noreply.github.com>
2024-10-11 10:02:16 +0900

    [Day15] skeleton 적용 (#16)
    * day15 - skeleton
    
    * Link 추가
    
    * 에러 처리 추가

--- src/app/(with-searchbar)/page.tsx
+++ src/app/(with-searchbar)/page.tsx
@@ -1,76 +1,60 @@
-import { MovieData } from "@/types";
-import style from "./page.module.css";
-import Link from "next/link";
 import MovieItem from "@/components/movie-item";
+import style from "./page.module.css";
+import { MovieData } from "@/types";
+import MovieListSkeleton from "@/components/skeleton/movie-list-skeleton";
+import { Suspense } from "react";
 
-async function RecommendMovie() {
+async function AllMovies() {
+  /* 새로운 영화를 추가하거나 삭제하지 않으므로 영구 캐싱 적용 */
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_MOVIE_API_URL}/movie`,
+    {
+      cache: "force-cache",
+    }
+  );
+  if (!response.ok) {
+    return <div>오류가 발생했습니다 ...</div>;
+  }
+
+  const allMovies: MovieData[] = await response.json();
+  return allMovies.map((movie) => (
+    <MovieItem key={`all-${movie.id}`} {...movie} />
+  ));
+}
+
+async function RecoMovies() {
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_MOVIE_API_URL}/movie/random`,
     { next: { revalidate: 10 } }
   );
-
   if (!response.ok) {
-    return <div>오류가 발생했습니다 ...</div>;
+    return <div>오류가 발생했습니다...</div>;
   }
 
-  const recommendMovie: MovieData[] = await response.json();
-
-  return (
-    <div className={style.movie_items}>
-      {recommendMovie.map((movie) => {
-        return (
-          <Link
-            href={`/movie/${movie.id}`}
-            key={movie.id}
-            className={style.recommend_movie_item}
-          >
-            <MovieItem {...movie} />
-          </Link>
-        );
-      })}
-    </div>
-  );
-}
-
-async function AllMovie() {
-  const response = await fetch(
-    `${process.env.NEXT_PUBLIC_MOVIE_API_URL}/movie`,
-    { cache: "force-cache" }
-  );
-
-  if (!response.ok) {
-    return <div>오류가 발생했습니다 ...</div>;
-  }
-
-  const allMovie: MovieData[] = await response.json();
-
-  return (
-    <div className={style.movie_items}>
-      {allMovie.map((movie) => {
-        return (
-          <Link
-            href={`/movie/${movie.id}`}
-            key={movie.id}
-            className={style.all_movie_item}
-          >
-            <MovieItem {...movie} />
-          </Link>
-        );
-      })}
-    </div>
-  );
+  const recoMovies: MovieData[] = await response.json();
+  return recoMovies.map((movie) => (
+    <MovieItem key={`reco-${movie.id}`} {...movie} />
+  ));
 }
 
 export default function Home() {
   return (
     <div className={style.container}>
       <section>
-        <h3>지금 가장 추천하는 영화</h3>
-        <RecommendMovie />
+        <h3>지금 추천하는 도서</h3>
+        <div className={style.reco_container}>
+          <Suspense fallback={<MovieListSkeleton count={3} />}>
+            <RecoMovies />
+          </Suspense>
+        </div>
       </section>
       <section>
-        <h3>등록된 모든 영화</h3>
-        <AllMovie />
+        <h3>등록된 모든 도서</h3>
+        <div className={style.all_container}>
+          <Suspense fallback={<MovieListSkeleton count={30} />}>
+            <AllMovies />
+          </Suspense>
+        </div>
       </section>
     </div>
   );
